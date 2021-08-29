131a8fbb049113e0204e02ca7cab19de742b6ae0
mbimbij <joseph.mbimbi@gmail.com>
2021-08-29 08:26:04 +0200

    Starting Point

--- src/main/java/org/example/usermanagement/EmailAddress.java
+++
@@ -1,9 +0,0 @@
-package org.example.usermanagement;
-
-import org.example.utils.ValueObject;
-
-public class EmailAddress extends ValueObject<String> {
-    public EmailAddress(String value) {
-        super(value);
-    }
-}
